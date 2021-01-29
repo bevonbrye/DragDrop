ea6b2ca7a755a01ad566d5b8b5308bd463f86d97
bevonbrye <bryebevon@gmail.com>
2021-01-29 12:57:15 -0800

    update drag functions

--- draganddrop/main.js
+++ draganddrop/main.js
@@ -1,1 +1,39 @@
-console.log('hello')
+const draggables = document.querySelectorAll('.draggable'); 
+const containers = document.querySelectorAll('.container'); 
+
+
+draggables.forEach(draggable => { 
+    draggable.addEventListener('dragstart',() => { 
+        draggable.classList.add('dragging')
+    })
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    })
+})
+
+containers.forEach(container => { 
+    container.addEventListener('dragover', (e) => { 
+        e.preventDefault()
+        const afterEl = getDragAfterEl(container, e.clientY); 
+        const draggable = document.querySelector('.dragging'); 
+        if(afterEl == null){
+        container.insertBefore(draggable, afterEl)
+        } else { 
+            container.appendChild(draggable)
+        }
+    })
+})
+
+function getDragAfterEl(container, y) {
+    const draggableEls = [...container.querySelectorAll('.draggable:not(.dragging)')]; 
+
+   return draggableEls.reduce((closest, child) => { 
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2; 
+        if(offset < 0 && offset > closest.offset) { 
+            return { offset: offset, element: child}
+        } else { 
+           return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY}).element
+}
